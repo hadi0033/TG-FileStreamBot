542736857e74cb4dd8fff2430ddd30333b300b79
hadi0033 <40006927+hadi0033@users.noreply.github.com>
2021-09-10 02:59:09 +0430

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -52,7 +52,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('------------------ Starting Keep Alive Service ------------------')
         print('\n')
-        let scheker = await ping_server,
+        scheker = 'await ping_server'
         scheduler = BackgroundScheduler()
         scheduler.add_job(scheker, "interval", seconds=1200)
         scheduler.start()
